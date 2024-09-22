50b403c2dda329c6f2a18648db87e1798959d377
Erik Cercasov <96184306+EClp007@users.noreply.github.com>
2024-09-22 23:46:27 +0200

    broadcast desktop position and roation

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,18 +1,27 @@
 import { Room, type Client } from "@colyseus/core";
-import { MyRoomState, Player, SharedSphere } from "./schema/MyRoomState";
+import {
+	MyRoomState,
+	Player,
+	SharedSphere,
+	Desktop,
+} from "./schema/MyRoomState";
+
 export class MyRoom extends Room<MyRoomState> {
 	maxClients = 4;
 
 	onCreate(options: any) {
 		this.setState(new MyRoomState());
 
+		// Handle position updates for the sphere
 		this.onMessage("updatePosition", (client, data) => {
 			const player = this.state.players.get(client.sessionId);
-			player.x = data.x;
-			player.y = data.y;
-			player.z = data.z;
+			if (player) {
+				player.x = data.x;
+				player.y = data.y;
+				player.z = data.z;
+			}
 
-			// Also update the shared sphere's position
+			// Update the shared sphere's position
 			this.state.sharedSphere.x = data.x;
 			this.state.sharedSphere.y = data.y;
 			this.state.sharedSphere.z = data.z;
@@ -24,28 +33,50 @@ export class MyRoom extends Room<MyRoomState> {
 				z: this.state.sharedSphere.z,
 			});
 		});
+
+		// Handle updates for the desktop
+		this.onMessage("updateDesktopTransform", (client, data) => {
+			this.state.desktop.x = data.position.x;
+			this.state.desktop.y = data.position.y;
+			this.state.desktop.z = data.position.z;
+			this.state.desktop.rotationX = data.rotation.x;
+			this.state.desktop.rotationY = data.rotation.y;
+			this.state.desktop.rotationZ = data.rotation.z;
+
+			// Broadcast the desktop's updated position and rotation to all clients
+			this.broadcast("updateDesktopTransform", {
+				position: {
+					x: this.state.desktop.x,
+					y: this.state.desktop.y,
+					z: this.state.desktop.z,
+				},
+				rotation: {
+					x: this.state.desktop.rotationX,
+					y: this.state.desktop.rotationY,
+					z: this.state.desktop.rotationZ,
+				},
+			});
+		});
 	}
 
 	onJoin(client: Client, options: any) {
 		console.log(client.sessionId, "joined!");
 
-		// create Player instance
+		// Create a Player instance
 		const player = new Player();
 
-		// place Player at a random position
+		// Place the Player at a random position
 		const FLOOR_SIZE = 500;
 		player.x = -(FLOOR_SIZE / 2) + Math.random() * FLOOR_SIZE;
 		player.y = -(FLOOR_SIZE / 2) + Math.random() * FLOOR_SIZE;
 		player.z = 0;
 
-		// place player in the map of players by its sessionId
-		// (client.sessionId is unique per connection!)
+		// Place player in the map of players by its sessionId
 		this.state.players.set(client.sessionId, player);
 	}
 
 	onLeave(client: Client, consented: boolean) {
 		console.log(client.sessionId, "left!");
-
 		this.state.players.delete(client.sessionId);
 	}
 
--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -8,15 +8,21 @@ export class Player extends Schema {
 
 export class SharedSphere extends Schema {
 	@type("number") x = 0;
-
 	@type("number") y = 0; // Start above the ground level
-
 	@type("number") z = 0;
+}
+
+export class Desktop extends Schema {
+	@type("number") x = 0;
+	@type("number") y = 0;
+	@type("number") z = 0;
+	@type("number") rotationX = 0;
+	@type("number") rotationY = 0;
+	@type("number") rotationZ = 0;
 }
 
 export class MyRoomState extends Schema {
 	@type({ map: Player }) players = new MapSchema<Player>();
-
-	@type(SharedSphere)
-	sharedSphere = new SharedSphere();
+	@type(SharedSphere) sharedSphere = new SharedSphere();
+	@type(Desktop) desktop = new Desktop();
 }
