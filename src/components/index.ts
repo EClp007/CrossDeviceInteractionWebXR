31e281bc62f5caa4853afb6442513c4ee37a7ae4
Erik Cercasov <96184306+EClp007@users.noreply.github.com>
2024-10-07 03:12:10 +0200

    Refactor import paths in index.ts: Remove export of createPortalMesh

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-export { createPortalMesh } from "./portal";
 export { initializeEngine } from "./engine";
 export { createDesktop } from "./desktop";
 export { createLight, createDirectionalLight } from "./ligtht";
